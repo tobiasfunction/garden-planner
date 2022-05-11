ae842d85e32946bfda09af830a57c11f082ea06a
tobiasfunction <tobiasfunction@protonmail.com>
2022-05-11 14:05:35 -0400

    Initial file creation

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.SQLite;
+
+
+namespace GardenBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLiteConnection con = new SQLiteConnection(@"Data Source = /home/tob/Documents/Academy/Code/Git/dbs/gardenbox.sqlite");
+            con.Open();
+            con.Close();
+        }
+    }
+}
